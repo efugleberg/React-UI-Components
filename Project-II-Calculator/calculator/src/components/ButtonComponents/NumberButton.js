76bb6e98b5af970b7a4e1bb6efecfcd0d6029b68
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-05-28 17:58:20 -0500

    MVP: messing around with arrays

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Button.css';
 
 
+
+
 const NumberButton = props => {
+    const Array = [];
+    for (let i=0; i < 10; i++) {
+    Array.push(i);
+    };
     return (
         <div>
     <button className={props.buttonStyle}> {props.text} </button> </div>
