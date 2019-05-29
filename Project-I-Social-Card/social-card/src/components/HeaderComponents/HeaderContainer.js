821cc6d575260bdcb3a31e0d6cec4304174c51df
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-05-28 21:17:32 -0500

    MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+
+function HeaderContainer() {
+    return (
+        <div>
+        <HeaderContent /> </div>
+    );
+}
+
+export default HeaderContainer;
